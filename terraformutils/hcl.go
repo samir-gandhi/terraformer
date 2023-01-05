5f3f026bd3d4fcf184ae7b150ca964bd54ff98fd
samir-gandhi <samirgandhi19@gmail.com>
2023-01-05 20:48:16 +0000

    remove tfer-- prefix

--- terraformutils/hcl.go
+++ terraformutils/hcl.go
@@ -277,7 +277,8 @@ func escapeRune(s string) string {
 // Sanitize name for terraform style
 func TfSanitize(name string) string {
 	name = unsafeChars.ReplaceAllStringFunc(name, escapeRune)
-	name = "tfer--" + name
+	// commented to remove the tfer-- prefix
+	// name = "tfer--" + name
 	return name
 }
 
